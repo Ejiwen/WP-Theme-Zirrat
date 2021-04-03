dcdd3e7022ffdc85fb094ee883b7c1ae6910a200
ejiwen <skibnewrim@gmail.com>
2021-04-02 19:03:33 -0700

    creat gulp task to simplify the bundle process

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,6 +7,7 @@ import sourcemaps from 'gulp-sourcemaps';
 import imagemin from 'gulp-imagemin';
 import del from 'del';
 import browserSync from 'browser-sync';
+import zip from 'gulp-zip';
 
 const server = browserSync.create();
 
@@ -14,7 +15,7 @@ const PRODUCTION = yargs.argv.prod;
 
 const paths = {
 	styles: {
-		src:'src/scss/bundle.scss',
+		src:['src/scss/bundle.scss','src/scss/admin.scss'],
 		dest: 'dist/assets/css'
 	},
   images: {
@@ -24,6 +25,15 @@ const paths = {
   scripts: {
     src:'src/js/index.js',
 		dest: 'dist/assets/js'
+  },
+  other: {
+    src:['src/**/*','!src/{images,js,scss}','!src/{images,js,scss}/**/*'],
+    dest: 'dist/assets'
+  },
+  package: {
+    src:['**/*', '!.vscode', '!node_modules{,/**}','!packaged{,/**', '!.babelrc','!.gitignore',
+    '!gulpfile.babel.js', 'package.json', '!package-lock.json','!webpack.config.js'],
+    dest:'packaged'
   }
 }
 
@@ -47,7 +57,8 @@ export const styles = () => {
     .pipe(sass().on('error', sass.logError))
     .pipe(gulpif(PRODUCTION, cleanCSS({compatibility: 'ie8'})))
     .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
-    .pipe(gulp.dest(paths.styles.dest));
+    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(server.stream());
 }
 
 export const images = () => {
@@ -57,10 +68,22 @@ export const images = () => {
 }
 
 export const watch = () => {
-	gulp.watch('src/scss/sass/**/*.scss', gulp.series(styles, reload))
+	// gulp.watch('src/scss/sass/**/*.scss', gulp.series(styles, reload))
+  gulp.watch('src/scss/sass/**/*.scss', styles)
   gulp.watch(paths.images.src, images)
 }
 
+export const copy = () => {
+  return gulp.src(paths.other.src)
+         .pipe(gulp.dest(paths.other.dest));
+}
 
-export const dev = gulp.series(clean, gulp.parallel(styles, images), serve, watch);
-export const build = gulp.series(clean, gulp.parallel(styles, images));
+export const compress = () => {
+  return gulp.src(paths.package.src)
+        .pipe(zip('zirrat.zip'))
+        .pipe(gulp.dest(paths.package.dest));
+}
+
+export const dev = gulp.series(clean, copy, gulp.parallel(styles, images), serve, watch);
+export const build = gulp.series(clean, copy, gulp.parallel(styles, images));
+export const bundle = gulp.series(build, compress);
--- inc/scripts-style.php
+++ inc/scripts-style.php
@@ -7,9 +7,9 @@ function zirrat_scripts() {
 add_action( 'wp_enqueue_scripts', 'zirrat_scripts' );
 
 
-// function zirrat_scripts_admin() {
-// 	$rand = rand(1, 999999999);
-//     wp_enqueue_style( 'zirrat-style', get_template_directory_uri() . '/dist/assets/css/admin.css', array(), $rand );
-// }
+function zirrat_scripts_admin() {
+	$rand = rand(1, 999999999);
+    wp_enqueue_style( 'zirrat-style', get_template_directory_uri() . '/dist/assets/css/admin.css', array(), $rand );
+}
 
-// add_action( 'admin_enqueue_scripts', 'zirrat_scripts_admin' );
+add_action( 'admin_enqueue_scripts', 'zirrat_scripts_admin' );
