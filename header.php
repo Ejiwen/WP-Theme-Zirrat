17970eb69376271e8d441b47363f774f91601f04
ejiwen <skibnewrim@gmail.com>
2021-04-05 11:06:06 -0700

    Automate the browser if any change detected

--- header.php
+++ header.php
@@ -1,10 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html <?php language_attributes(); ?>>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<?php wp_head(); ?>
 </head>
-<body>
-<?php wp_head() ?>
+<body <?php body_class(); ?>>
